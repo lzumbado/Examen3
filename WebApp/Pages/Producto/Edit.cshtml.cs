1e664001768b85ecf0417fc6b46b92d0c12ca8ff
LEONARDO JOSUE ZUMBADO <leozch11@gmail.com>
2021-12-18 00:02:02 -0600

    Razor Producto

--- WebApp/Pages/Producto/Edit.cshtml.cs
+++ WebApp/Pages/Producto/Edit.cshtml.cs
@@ -4,13 +4,58 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Entity;
+using WBL;
 
 namespace WebApp.Pages.Producto
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly ServiceApi service;
+
+        public EditModel(ServiceApi service)
         {
+            this.service = service;
         }
+
+
+
+
+        [BindProperty]
+        public ProductoEntity Entity { get; set; } = new ProductoEntity();
+
+
+        [BindProperty(SupportsGet = true)]
+        public int? id { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            try
+            {
+                if (id.HasValue)
+                {
+                    Entity = await service.ProductoGetById(id.Value);
+                }
+
+
+
+                return Page();
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
+
+
+        }
+
+
+
+
+
+
+
+
     }
 }
