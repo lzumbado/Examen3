00ebd265bd1b755ce56b68364b3bdb6b51fb9264
LEONARDO JOSUE ZUMBADO <leozch11@gmail.com>
2021-12-18 00:21:55 -0600

    Compra Grid razro

--- WebApp/Pages/Compra/Grid.cshtml.cs
+++ WebApp/Pages/Compra/Grid.cshtml.cs
@@ -4,13 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WBL;
+using Entity;
 
 namespace WebApp.Pages.Compra
 {
     public class GridModel : PageModel
     {
-        public void OnGet()
+        private readonly ServiceApi service;
+
+        public GridModel(ServiceApi service)
         {
+            this.service = service;
+        }  
+
+
+        public IEnumerable<CompraEntity> GridList { get; set; } = new List<CompraEntity>();
+
+
+        public async Task<IActionResult> OnGet()
+        {
+
+            try
+            {
+                GridList = await service.CompraGet();
+
+                return Page();
+
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
+
         }
+   
     }
 }
