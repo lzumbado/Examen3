c23405a9088d6c69c09807908292158f5bf6f88e
LEONARDO JOSUE ZUMBADO <leozch11@gmail.com>
2021-12-17 20:05:28 -0600

    Razor Pages

--- WebApp/Pages/Clientes/Grid.cshtml.cs
+++ WebApp/Pages/Clientes/Grid.cshtml.cs
@@ -4,13 +4,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WBL;
+using Entity;
 
 namespace WebApp.Pages.Clientes
 {
     public class GridModel : PageModel
     {
-        public void OnGet()
+        private readonly ServiceApi service;
+
+        public GridModel(ServiceApi service)
         {
+            this.service = service;
         }
+
+
+       
+
+        public IEnumerable<ClientesEntity> GridList { get; set; } = new List<ClientesEntity>();
+
+
+        public async Task<IActionResult> OnGet()
+        {
+
+            try
+            {
+                GridList = await service.ClientesGet();
+
+                return Page();
+
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
+
+        }
+      
+
     }
 }
